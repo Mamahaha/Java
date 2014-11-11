2f382fe8293b35febbe363bb6723b98f8b022e0e
mamahaha <led@localhost.localdomain>
2014-11-11 21:41:43 +0800

    Add a new hadoop project : word_count

---
+++ wordcount/src/main/java/com/ericsson/led/github/hadoop/word_count/WordCountMapper.java
@@ -0,0 +1,21 @@
+package com.ericsson.led.github.hadoop.word_count;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
+	private final IntWritable ONE = new IntWritable(1);
+	private Text word = new Text();
+	
+	public void map(Object key, Text value, Context ctx) throws IOException, InterruptedException {
+		String[] csv = value.toString().split(" ");
+		for (String str : csv) {
+			word.set(str);
+			ctx.write(word, ONE);
+		}
+	}
+	
+}
