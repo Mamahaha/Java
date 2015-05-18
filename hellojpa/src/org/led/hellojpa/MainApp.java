a23af9f6c25e27a2996ea3966f33a96107946a84
led <led.xu@ericsson.com>
2015-05-18 14:06:26 +0800

    Update query logic

--- hellojpa/src/org/led/hellojpa/MainApp.java
+++ hellojpa/src/org/led/hellojpa/MainApp.java
@@ -1,11 +1,17 @@
 package org.led.hellojpa;
 
+import java.util.List;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
 import org.led.hellojpa.dao.GuildDao;
+import org.led.hellojpa.dao.PlayerDao;
 import org.led.hellojpa.dao.impl.GuildDaoImpl;
+import org.led.hellojpa.dao.impl.PlayerDaoImpl;
 import org.led.hellojpa.entity.GuildEntity;
+import org.led.hellojpa.entity.PlayerEntity;
 import org.led.hellojpa.utils.EntityManagerUtil;
 import org.led.hellojpa.utils.PersistenceContext;
 
@@ -24,12 +30,12 @@ public class MainApp {
 		GuildDao dao = new GuildDaoImpl(ctx);
 		
 		GuildEntity entity = new GuildEntity();
-		entity.setName("GOD");
-		entity.setCount(10);
-		entity.setLevel(3);
-		entity.setFlag("GOD Flag");
+		entity.setName("DEVIL");
+		entity.setCount(6);
+		entity.setLevel(8);
+		entity.setFlag("DEVIL Flag");
 		
-		dao.createGuildDao(entity);
+		dao.createGuildEntity(entity);
 		et.commit();
 		
 		if (ctx != null) {
@@ -46,23 +52,47 @@ public class MainApp {
 		EntityTransaction et = ctx.getEntityManager().getTransaction();
 		et.begin();
 		
-		GuildDao dao = new GuildDaoImpl(ctx);
+		PlayerDao pDao = new PlayerDaoImpl(ctx);
+		GuildDao gDao = new GuildDaoImpl(ctx);
 		
-		GuildEntity entity = new GuildEntity();
-		entity.setName("GOD");
-		entity.setCount(10);
-		entity.setLevel(3);
-		entity.setFlag("GOD Flag");
+		PlayerEntity entity = new PlayerEntity();
+		entity.setName("Moon");
+		entity.setAge(15);
+		entity.setLevel(18);
+		entity.setScore(378960);
+		entity.setSex("Male");
+		entity.setGid(gDao.getIdByName("DEVIL"));
 		
-		dao.createGuildDao(entity);
+		pDao.createPlayerEntity(entity);
 		et.commit();
 		
 		if (ctx != null) {
 			ctx.getEntityManager().close();
         }
 	}
+	
+	public static void testSqlCmd(String sqlCmd) {
+		PersistenceContext ctx = new PersistenceContext();
+		EntityManagerFactory factory = EntityManagerUtil
+                .getEntityManagerFactory();
+		
+		ctx.setEntityManager(factory.createEntityManager());
+		Query query = ctx.getEntityManager().createQuery(sqlCmd);
+		@SuppressWarnings("unchecked")
+		List<GuildEntity> list = (List<GuildEntity>)query.getResultList();
+		for (GuildEntity entity : list) {
+			System.out.println(entity.toString());
+		}
+		
+		if (ctx != null) {
+			ctx.getEntityManager().close();
+        }
+	}
 	public static void main(String[] args) {
-		addGuild();
-		//addPlayer();
+		//addGuild();
+		addPlayer();
+		
+		//==============
+		//testSqlCmd("SELECT a FROM GuildEntity a");
 	}
 }
