67443a53fb36d7b67e44a3c7bfeaef4f9c94f6cb
led <led.xu@ericsson.com>
2015-12-16 17:06:17 +0800

    remove sensitive word

--- phonebook/src/main/java/org/led/exercise/mypb/Inputter.java
+++ phonebook/src/main/java/org/led/exercise/mypb/Inputter.java
@@ -1,18 +1,3 @@
-/*
- *             File : Inputter.java
- *        Classname : Inputter
- *        Author(s) : Led Xu
- *          Created : 2012-3-23
- * 
- * Copyright (c) 2012 Ericsson AB, Sweden.
- * All rights reserved.
- * The Copyright to the computer program(s) herein is the property of
- * Ericsson AB, Sweden.
- * The program(s) may be used and/or copied with the written permission
- * from Ericsson AB or in accordance with the terms
- * and conditions stipulated in the agreement/contract under which the
- * program(s) have been supplied.
- */
 
 // Package
 package org.led.exercise.mypb;
--- phonebook/src/main/java/org/led/exercise/mypb/PhoneRecord.java
+++ phonebook/src/main/java/org/led/exercise/mypb/PhoneRecord.java
@@ -1,18 +1,4 @@
-/*
- *             File : PhoneRecord.java
- *        Classname : PhoneRecord
- *        Author(s) : Led Xu
- *          Created : 2012-3-23
- * 
- * Copyright (c) 2012 Ericsson AB, Sweden.
- * All rights reserved.
- * The Copyright to the computer program(s) herein is the property of
- * Ericsson AB, Sweden.
- * The program(s) may be used and/or copied with the written permission
- * from Ericsson AB or in accordance with the terms
- * and conditions stipulated in the agreement/contract under which the
- * program(s) have been supplied.
- */
+
  
 
 package org.led.exercise.mypb;
