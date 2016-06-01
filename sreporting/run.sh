2eef86dd289ed7c5183a76bc03cb15f7dc546993
exuyufe <led.xu@ericsson.com>
2016-06-01 16:06:33 +0800

    add avro handling

--- sreporting/run.sh
+++ sreporting/run.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+
+#=============Generate java code for avro file==========
+#$ java -jar /opt/apache-avro/default/avro-tools-1.7.6.jar compile schema ./common/src/main/resources/receptionreport.avsc .
+#=============de-serialize an avro file============
+#$ java -jar /opt/apache-avro/default/avro-tools-1.7.6.jar tojson test.avro > test.json
+#=============get schema from avro file===========
+#$ java -jar /opt/apache-avro/default/avro-tools-1.7.6.jar getschema test.avro > test.avsc
+#=============serialize a json file===============
+#$ java -jar /opt/apache-avro/default/avro-tools-1.7.6.jar fromjson --schema-file test.avsc test.json
+
+
 #java -jar /opt/apache-avro/default/avro-tools-1.7.6.jar compile schema receptionreport.avsc src/main/java/
 mvn clean install
 jar umf src/META-INF/MANIFEST.MF target/sreporting-0.0.1-SNAPSHOT.jar
